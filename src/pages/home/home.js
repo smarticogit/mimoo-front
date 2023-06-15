bb308c36fd92b5afc9174c589fd01ad432e25e9b
Daniel <daniel.repres@hotmail.com>
2023-06-15 11:34:37 -0300

    refactor: update menu designer

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,7 +1,22 @@
 const list = document.querySelector('.list');
 const create = document.querySelector('.create');
+const iconMenuLogo = document.querySelector('.icon-menu-logo');
+const openClosed = document.querySelectorAll('.box-icon-closed')
+const menuH2 = document.querySelectorAll('h2');
+
 import { serviceList } from '../../service/serviceList.js'
 import { serviceCreate } from '../../service/serviceCreate.js';
+
+iconMenuLogo.addEventListener('click', () => {
+
+        menuH2.forEach(elemento => {
+            elemento.classList.toggle('hidden');
+        });
+
+        openClosed.forEach(elemento => {
+            elemento.classList.toggle('box-icon-open');
+        });
+});
 
 list.addEventListener('click', () => {
 
